e4fa67bde92afc04ad6c78724158282fa63ace9e
mayingbing <mayingbing@163.com>
2016-05-11 14:55:42 +0800

    511

---
+++ application/config/config.php
@@ -0,0 +1,11 @@
+<?php
+//项目配置文件
+return array(
+	'host' => 'localhost',
+	'user' => 'root',
+	'password' => 'ma151626',
+	'dbname' => 'ma_shop',
+	'port' => '3306',
+	'charset' => 'utf8',
+	'prefix' => 'cz_'
+);
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//入口文件
+//引入核心启动类
+include "framework/core/Framework.class.php";
+
+Framework::run();
+
+
+
